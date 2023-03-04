df1e81a44bb0d16a3dd501d8bebb1043d61479b4
Krissada Singhakachain <omsin_47@hotmail.com>
2023-03-04 06:49:42 +0000

    add export

--- run_once_before_install-omz.sh
+++ run_once_before_install-omz.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
-CHSH=yes
-RUNZSH=no
-KEEP_ZSHRC=yes
+export CHSH=yes
+export RUNZSH=no
+export KEEP_ZSHRC=yes
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
